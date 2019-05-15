9becdb5dc6fd8e5b3a6ed9a0b97d3ddb124be5b3
mohammad katoozi <k2527806@gmail.com>
2019-05-15 14:18:30 +0430

    create settings.py for get use configuration

---
+++ handypackages/settings.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+
+TEXT_PHRASE_MODEL_PHRASE_TYPES = getattr(
+    settings, "TEXT_PHRASE_MODEL_PHRASES_TYPES", [])
+
+TEXT_PHRASE_SIMPLE_CONTEXT_OBJECT_NAME = getattr(
+    settings, "TEXT_PHRASE_SIMPLE_CONTEXT_OBJECT_NAME", "text_phrases")
+
+TEXT_PHRASE_LANG_CONTEXT_OBJECT_NAME = getattr(
+    settings, "TEXT_PHRASE_LANG_CONTEXT_OBJECT_NAME", "text_phrases_lang")
+
+
+def _get_languages():
+    """
+    generate language list with Global Key
+    for language independent phrases
+    """
+    language_list = [
+        ("global", 'Global')
+    ]
+    for item in getattr(settings, "LANGUAGES", []):
+        language_list.append(item)
+    return language_list
+
+
+TEXT_PHRASE_LANGUAGES = _get_languages

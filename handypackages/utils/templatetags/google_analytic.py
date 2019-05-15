94b2962d276d6555839f897c2c500da3925a2b0a
mohammad katoozi <k2527806@gmail.com>
2019-05-15 13:19:16 +0430

    create google_analytic template tag for generat google analytic html

---
+++ handypackages/utils/templatetags/google_analytic.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("handyutils/google-analytics.html")
+def google_analytic(api_key=None):
+    """
+    generate google analytic html
+    example:
+        {% load google_analytic %}
+        {% google_analytic "UA-111111111" %}
+    """
+    if not api_key:
+        raise Exception("google api key is required.")
+    return {
+        "api_key": api_key
+    }

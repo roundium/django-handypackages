4564e4bf384e4926b1066cb25db43645e6ce1173
mrouhi13 <mrouhi13@icloud.com>
2019-05-18 18:26:26 +0430

    Update urls.py

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,6 +1,6 @@
-# from django.conf import settings
+from django.conf import settings
 from django.conf.urls import include, url
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls'))
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

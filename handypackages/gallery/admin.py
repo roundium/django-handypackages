586398c739579167a7f705b93c7b5cc7738fc826
mohammad katoozi <k2527806@gmail.com>
2019-06-30 15:00:37 +0430

    add language to gallery app

--- handypackages/gallery/admin.py
+++ handypackages/gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .forms import GalleryForm
 from .models import Gallery
 
 
@@ -7,8 +8,9 @@ class GalleryAdminPanel(admin.ModelAdmin):
     list_display = ['title', 'create_time']
     list_filter = ['create_time']
     filter_horizontal = ('tags', )
-    fields = ['title', 'text', 'image', 'tags', 'create_time']
+    fields = ['title', 'text', 'image', 'language', 'tags', 'create_time']
     readonly_fields = ['create_time']
+    form = GalleryForm
 
 
 admin.site.register(Gallery, GalleryAdminPanel)
---
+++ handypackages/gallery/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from easy_select2.widgets import Select2
+
+from handypackages.settings import ALL_LANGUAGES
+
+from .models import Gallery
+
+
+class GalleryForm(forms.ModelForm):
+    language = forms.ChoiceField(choices=ALL_LANGUAGES, widget=Select2())
+
+    class Meta:
+        model = Gallery
+        fields = '__all__'
